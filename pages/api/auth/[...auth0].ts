c7ea2f692b143b88b59610a0bdaf554e37dd4b31
watanabe <sek.ohei.w0822@icloud.com>
2021-09-27 00:45:13 +0900

    change: auth0_base_url

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -1,3 +1,4 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+import { initAuth0 } from '@auth0/nextjs-auth0';
 
-export default handleAuth();
+const baseURL = process.env.VERCEL_URL || process.env.AUTH0_BASE_URL;
+export default initAuth0({ baseURL }).handleAuth();
