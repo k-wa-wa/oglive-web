41bc002eda9ccc63f6f10265af83f118b3d6b178
watanabe <sek.ohei.w0822@icloud.com>
2021-09-27 15:05:23 +0900

    change: 微修正

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-
+[![](https://img.shields.io/badge/production-up-brightgreen)](https://hit-unplugged-oglive.vercel.app/)
+[![](https://img.shields.io/badge/development-up-brightgreen)](https://hit-unplugged-oglive-git-dev-ko-he-e.vercel.app/)
 
 ※本当はGitHub Organizationsに置きたかったのですが、OrganizationからVercelにデプロイするには課金してVercel Teamを作らなきゃいけないとのことなので、個人のリポジトリに配置しました。ごめんなさいいいいい。
--- components/LiveVolume/Contact/ContactBody.tsx
+++ components/LiveVolume/Contact/ContactBody.tsx
@@ -1,8 +1,25 @@
 
+import Reveal from "@/components/Utils/Animation/Reveal";
+import Error from "@/components/Utils/Error";
+import LayoutBox from "@/components/Utils/LayoutComponent/LayoutBox";
+import PageTitle from "@/components/Utils/LayoutComponent/PageTitle";
+import LoadingBody from "@/components/Utils/Loading/LoadingBody";
+import MdViewer from "@/components/Utils/Markdown/MdViewer";
 
 const ContactBody = () => {
+  const body = `
+  作成中
+  `;
   return (
-    <div></div>
+    <div>
+      <Reveal animVars={{ y: 20 }}>
+        <PageTitle title="お問い合わせ" />
+      </Reveal>
+
+      <Reveal>
+        <MdViewer body={body} />
+      </Reveal>
+    </div>
   );
 };
 
