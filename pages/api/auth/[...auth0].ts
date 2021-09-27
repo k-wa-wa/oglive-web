62a44e81dc909d8ba4bd0b240cc7ec49626cbad0
watanabe <sek.ohei.w0822@icloud.com>
2021-09-27 14:00:57 +0900

    test

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -2,6 +2,7 @@ import { initAuth0 } from '@auth0/nextjs-auth0';
 
 const handler = () => {
   const baseURL = process.env.VERCEL_URL || process.env.AUTH0_BASE_URL;
+  console.log(baseURL);
   return initAuth0({ baseURL: `https://${process.env.VERCEL_URL}` }).handleAuth();
 };
 
