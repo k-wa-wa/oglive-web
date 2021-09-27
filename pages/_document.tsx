65e22757f92d86d76f4887b8fe426416512ee8d3
watanabe <sek.ohei.w0822@icloud.com>
2021-09-27 14:10:15 +0900

    Revert "fix: font-displayをswapに変更"
    This reverts commit b8203b663a3744e4e2ff54a6e5841761260b5237.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,13 +6,11 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Alegreya+Sans+SC:wght@300&display=swap"
+            href="https://fonts.googleapis.com/css?family=Alegreya+Sans+SC:300&display=optional"
             rel="stylesheet" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Sawarabi+Mincho&display=swap"
+            href="https://fonts.googleapis.com/css?family=Sawarabi+Mincho&display=optional"
             rel="stylesheet" />
         </Head>
         <body>
