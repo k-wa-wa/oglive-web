ef4389f3aae55cb7a98671e96ce9047817ece102
watanabe <sek.ohei.w0822@icloud.com>
2021-11-22 20:52:25 +0900

    update: splideの導入

--- components/LiveVolume/Home/Gallery.tsx
+++ components/LiveVolume/Home/Gallery.tsx
@@ -1,21 +1,35 @@
+import { Splide, SplideSlide } from '@splidejs/react-splide';
 import Image from "next/image";
 import Error from "@/components/Utils/Error";
 import LoadingBody from "@/components/Utils/Loading/LoadingBody";
 import { useSwrReadCmsAssetList } from "@/modules/swr";
+
+import '@splidejs/splide/dist/css/splide.min.css';
+
 
 type Props = {
   liveVolumeN: number | undefined;
 }
 const Gallery: React.VFC<Props> = (props) => {
   const { data, isLoading, isError } = useSwrReadCmsAssetList(props.liveVolumeN);
+
   if (isLoading) return <LoadingBody />;
   if (isError) return <Error />;
+
+  const imgWidth = 400;
+  const imgHeight = 250;
+
   return (
-    <div>
+    <Splide options={{
+      type: "loop",
+      padding: "15rem"
+    }}>
       {data.map(d => (
-        <Image key={d.url} src={d.url} width={600} height={300} alt="staff" />
+        <SplideSlide key={d.url}>
+          <Image key={d.url} src={d.url} width={imgWidth} height={imgHeight} alt="staff" />
+        </SplideSlide>
       ))}
-    </div>
+    </Splide>
   );
 };
 
