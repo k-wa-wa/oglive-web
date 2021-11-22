031a451518de2402299260c73cf89a0266b339d4
watanabe <sek.ohei.w0822@icloud.com>
2021-11-22 13:57:56 +0900

    change: revealコンポーネントのアニメーション設定を変更

--- components/Utils/Animation/Reveal.tsx
+++ components/Utils/Animation/Reveal.tsx
@@ -1,10 +1,13 @@
-import { useEffect, useRef, useState } from "react"; 
+import { useEffect, useRef, useState } from "react";
 
 import styles from "./Reveal.module.scss";
 
+type _translateStr = `translate(${number}${"" | "rem" | "%"}, ${number}${"" | "rem" | "%"})`;
+type _scaleStr = `scale(${number}, ${number})`;
 type Props = {
   children: React.ReactNode;
-  transformFrom?: `translate(${number}${"" | "rem" | "%"}, ${number}${"" | "rem" | "%"})`;
+  transformFrom: _translateStr | _scaleStr
+  transformTo?: _translateStr | _scaleStr
 }
 
 const Reveal: React.FC<Props> = (props) => {
@@ -13,13 +16,13 @@ const Reveal: React.FC<Props> = (props) => {
 
   const defaultStyle: React.CSSProperties = {
     opacity: 0,
-    transform: props.transformFrom ?? ""
+    transform: props.transformFrom
   };
 
   const activeStyle: React.CSSProperties = {
     ...defaultStyle,
     opacity: 1,
-    transform: "translate(0, 0)",
+    transform: props.transformTo ?? "translate(0, 0)",
     transition: "all 1s ease-in-out"
   };
 
